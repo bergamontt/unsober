7e816a9e63f842f7bfd6dae2307d8c155a696209
bergamontt <dariamumbo@gmail.com>
2025-09-09 10:18:22 +0300

    Added .xlsx schedule parser

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,6 +21,10 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.projectlombok:lombok:1.18.40")
+    implementation("org.apache.poi:poi:5.4.1")
+    implementation("org.apache.poi:poi-ooxml:5.2.5")
+    implementation("org.apache.logging.log4j:log4j-core:2.25.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
