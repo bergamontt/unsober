b90f9776bec7bcd9c1fedcecd7eb96f762d2dfd4
Daria <da.bulavina@ukma.edu.ua>
2025-09-10 09:25:20 +0300

    Lombok annotation processor fix

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,7 +21,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.projectlombok:lombok:1.18.40")
+    compileOnly("org.projectlombok:lombok:1.18.40")
+    annotationProcessor("org.projectlombok:lombok:1.18.40")
     implementation("org.apache.poi:poi:5.4.1")
     implementation("org.apache.poi:poi-ooxml:5.2.5")
     implementation("org.apache.logging.log4j:log4j-core:2.25.0")
