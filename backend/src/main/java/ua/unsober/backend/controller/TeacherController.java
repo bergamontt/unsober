f73edde8af63998fc41d2c9b4f5a7369f3da4f06
Stellar-Nucleosynthesis <stepanenkonazar356@gmail.com>
2025-09-23 18:37:01 +0300

    Added basic EntityNotFoundException handling

--- backend/src/main/java/ua/unsober/backend/controller/TeacherController.java
+++ backend/src/main/java/ua/unsober/backend/controller/TeacherController.java
@@ -1,33 +1,23 @@
 
 package ua.unsober.backend.controller;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import ua.unsober.backend.dtos.request.TeacherRequestDto;
 import ua.unsober.backend.dtos.response.TeacherResponseDto;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/teachers")
 public class TeacherController {
+    private final List<TeacherResponseDto> teachers = new ArrayList<>();
 
-    @PostMapping
-    public TeacherResponseDto create(@Valid @RequestBody TeacherRequestDto dto) {
-        return TeacherResponseDto.builder()
-                .id(UUID.randomUUID())
-                .firstName(dto.getFirstName())
-                .lastName(dto.getLastName())
-                .patronymic(dto.getPatronymic())
-                .email(dto.getEmail())
-                .build();
-    }
-
-    @GetMapping
-    public List<TeacherResponseDto> getAll() {
-        return List.of(
+    TeacherController(){
+        teachers.add(
                 TeacherResponseDto.builder()
                         .id(UUID.randomUUID())
                         .firstName("Олександр")
@@ -38,31 +28,55 @@ public class TeacherController {
         );
     }
 
+    @PostMapping
+    public TeacherResponseDto create(@Valid @RequestBody TeacherRequestDto dto) {
+        TeacherResponseDto response = TeacherResponseDto.builder()
+                .id(UUID.randomUUID())
+                .firstName(dto.getFirstName())
+                .lastName(dto.getLastName())
+                .patronymic(dto.getPatronymic())
+                .email(dto.getEmail())
+                .build();
+        teachers.add(response);
+        return response;
+    }
+
+    @GetMapping
+    public List<TeacherResponseDto> getAll() {
+        return teachers;
+    }
+
     @GetMapping("/{id}")
     public TeacherResponseDto getById(@PathVariable UUID id) {
-        return TeacherResponseDto.builder()
-                .id(id)
-                .firstName("Олександр")
-                .lastName("Олексієнко")
-                .patronymic("Олександрович")
-                .email("olexandr.olexienko@ukma.edu.ua")
-                .build();
+        for (TeacherResponseDto teacher : teachers) {
+            if (teacher.getId().equals(id))
+                return teacher;
+        }
+        throw new EntityNotFoundException("Teacher with id " + id + " not found");
     }
 
     @PatchMapping("/{id}")
     public TeacherResponseDto update(
             @PathVariable UUID id,
             @RequestBody TeacherRequestDto dto) {
-        return TeacherResponseDto.builder()
-                .id(id)
-                .firstName(Optional.ofNullable(dto.getFirstName()).orElse("Олександр"))
-                .lastName(Optional.ofNullable(dto.getLastName()).orElse("Олексієнко"))
-                .patronymic(Optional.ofNullable(dto.getPatronymic()).orElse("Олександрович"))
-                .email(Optional.ofNullable(dto.getEmail()).orElse("olexandr.olexienko@ukma.edu.ua"))
-                .build();
+        TeacherResponseDto teacher = getById(id);
+        if(dto.getFirstName() != null)
+            teacher.setFirstName(dto.getFirstName());
+        if(dto.getLastName() != null)
+            teacher.setLastName(dto.getLastName());
+        if(dto.getPatronymic() != null)
+            teacher.setPatronymic(dto.getPatronymic());
+        if(dto.getEmail() != null)
+            teacher.setEmail(dto.getEmail());
+        return teacher;
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable UUID id) {}
+    public void delete(@PathVariable UUID id) {
+        if (teachers.stream().anyMatch(teacher -> teacher.getId().equals(id)))
+            teachers.removeIf(teacher -> teacher.getId().equals(id));
+        else
+            throw new EntityNotFoundException("Teacher with id " + id + " not found");
+    }
 
 }
