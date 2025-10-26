3e3e26f0bb0773320f0d65dd98a57086bad75638
Stellar-Nucleosynthesis <stepanenkonazar356@gmail.com>
2025-10-26 13:24:16 +0200

    Minor authentication bug fix

--- backend/src/main/java/ua/unsober/backend/feature/auth/JwtTokenService.java
+++ backend/src/main/java/ua/unsober/backend/feature/auth/JwtTokenService.java
@@ -2,12 +2,14 @@ package ua.unsober.backend.feature.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.*;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,10 +19,14 @@ public class JwtTokenService {
 
     public String generateToken(Authentication auth) {
         Instant now = Instant.now();
+        List<String> roles = auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuedAt(now)
                 .expiresAt(now.plus(1, ChronoUnit.HOURS))
                 .subject(auth.getName())
+                .claim("roles", roles)
                 .build();
         JwtEncoderParameters params = JwtEncoderParameters.from(
                 JwsHeader.with(MacAlgorithm.HS256).build(), claims);
