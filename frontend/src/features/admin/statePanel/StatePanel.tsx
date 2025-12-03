94b6bb2eb8f39d3c8e9defe66e65bff42f7801a4
Tymko <kondrashko13@proton.me>
2025-12-03 10:56:28 +0200

    fix admin state page centered

--- frontend/src/features/admin/statePanel/StatePanel.tsx
+++ frontend/src/features/admin/statePanel/StatePanel.tsx
@@ -1,16 +1,16 @@
-import { Button, Group, NativeSelect, Space, Stack, Title } from "@mantine/core";
-import { useCallback, useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { getAppState, setCurrentStage, setCurrentTerm, setCurrentYear } from "../../../services/AppStateService";
+import {Button, Center, Group, NativeSelect, Space, Stack, Title} from "@mantine/core";
+import {useCallback, useEffect, useState} from "react";
+import {useTranslation} from "react-i18next";
+import {getAppState, setCurrentStage, setCurrentTerm, setCurrentYear} from "../../../services/AppStateService";
 import useFetch from "../../../hooks/useFetch";
-import { Term } from "../../../models/Subject";
-import { EnrollmentStage } from "../../../models/AppState";
-import { notifications } from "@mantine/notifications";
+import {Term} from "../../../models/Subject";
+import {EnrollmentStage} from "../../../models/AppState";
+import {notifications} from "@mantine/notifications";
 import axios from "axios";
 
 function StatePanel() {
-    const { t } = useTranslation("manageState");
-    const { data: state } = useFetch(getAppState, []);
+    const {t} = useTranslation("manageState");
+    const {data: state} = useFetch(getAppState, []);
     const [year, setYear] = useState<number | undefined>(state?.currentYear);
     const [term, setTerm] = useState<Term | undefined>(state?.term);
     const [stage, setStage] = useState<EnrollmentStage | undefined>(state?.enrollmentStage);
@@ -74,64 +74,70 @@ function StatePanel() {
     }, [t, state, year, term, stage]);
 
     const range = (start: number, end: number): number[] =>
-        Array.from({ length: end - start + 1 }, (_, i) => start + i);
+        Array.from({length: end - start + 1}, (_, i) => start + i);
     const years = range(2000, 2050);
 
     return (
-        <Group >
-            <Stack gap="xs">
+        <Group>
+            <Stack gap="xs" w={"100%"}>
                 <Title order={4}>
                     {t("enrollmentStateManagement")}
                 </Title>
-                <Space />
-                <Title order={4} c="dimmed">
-                    {t("selectYear")}
-                </Title>
-                <NativeSelect
-                    description={t("thisYearUsed")}
-                    data={years.map((year) => ({
-                        value: String(year),
-                        label: year + "-" + (year + 1)
-                    }))}
-                    value={year}
-                    onChange={(e) => setYear(Number(e.currentTarget.value))}
-                />
-                <Title order={4} c="dimmed">
-                    {t("selectTerm")}
-                </Title>
-                <NativeSelect
-                    description={t("thisTermUsed")}
-                    data={[
-                        { value: Term.AUTUMN, label: t(Term.AUTUMN) },
-                        { value: Term.SPRING, label: t(Term.SPRING) },
-                        { value: Term.SUMMER, label: t(Term.SUMMER) },
-                    ]}
-                    value={term}
-                    onChange={(e) => setTerm(e.currentTarget.value as Term)}
-                />
-                <Title order={4} c="dimmed">
-                    {t("selectStage")}
-                </Title>
-                <NativeSelect
-                    data={[
-                        { value: EnrollmentStage.CLOSED, label: t(EnrollmentStage.CLOSED) },
-                        { value: EnrollmentStage.COURSES, label: t(EnrollmentStage.COURSES) },
-                        { value: EnrollmentStage.GROUPS, label: t(EnrollmentStage.GROUPS) },
-                        { value: EnrollmentStage.CORRECTION, label: t(EnrollmentStage.CORRECTION) },
-                    ]}
-                    value={stage}
-                    onChange={(e) => setStage(e.currentTarget.value as EnrollmentStage)}
-                />
-                <Button
-                    variant="filled"
-                    color="green"
-                    mt="xs"
-                    onClick={handleSave}
-                    loading={isSaving}
-                    disabled={isSaving}
-                >
-                    {t("saveChanges")}
-                </Button>
+                <Space/>
+                <Center>
+                    <Stack w={"400px"}>
+                        <Title order={4} c="dimmed">
+                            {t("selectYear")}
+                        </Title>
+                        <NativeSelect
+                            description={t("thisYearUsed")}
+                            data={years.map((year) => ({
+                                value: String(year),
+                                label: year + "-" + (year + 1)
+                            }))}
+                            value={year}
+                            onChange={(e) => setYear(Number(e.currentTarget.value))}
+                        />
+                        <Title order={4} c="dimmed">
+                            {t("selectTerm")}
+                        </Title>
+                        <NativeSelect
+                            description={t("thisTermUsed")}
+                            data={[
+                                {value: Term.AUTUMN, label: t(Term.AUTUMN)},
+                                {value: Term.SPRING, label: t(Term.SPRING)},
+                                {value: Term.SUMMER, label: t(Term.SUMMER)},
+                            ]}
+                            value={term}
+                            onChange={(e) => setTerm(e.currentTarget.value as Term)}
+                        />
+                        <Title order={4} c="dimmed">
+                            {t("selectStage")}
+                        </Title>
+                        <NativeSelect
+                            data={[
+                                {value: EnrollmentStage.CLOSED, label: t(EnrollmentStage.CLOSED)},
+                                {value: EnrollmentStage.COURSES, label: t(EnrollmentStage.COURSES)},
+                                {value: EnrollmentStage.GROUPS, label: t(EnrollmentStage.GROUPS)},
+                                {value: EnrollmentStage.CORRECTION, label: t(EnrollmentStage.CORRECTION)},
+                            ]}
+                            value={stage}
+                            onChange={(e) => setStage(e.currentTarget.value as EnrollmentStage)}
+                        />
+                        <Button
+                            variant="filled"
+                            color="green"
+                            mt="xs"
+                            onClick={handleSave}
+                            loading={isSaving}
+                            disabled={isSaving}
+                        >
+                            {t("saveChanges")}
+                        </Button>
+                    </Stack>
+
+                </Center>
+
             </Stack>
         </Group>
     );
